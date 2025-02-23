a9695c787d2e8b1afb2166dbe11a01ee8ca2e20f
Walthier David <walthierdavid@edu.bme.hu>
2025-02-23 17:55:43 +0100

    Corrected pylint

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
+"""A simple Flask web server that responds with a greeting."""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
+    """Handle GET requests to root URL by returning a greeting."""
     return "Hello there!"
 
 if __name__ == '__main__':
